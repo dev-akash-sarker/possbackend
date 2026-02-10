2a158dcb33a221103c33b69549f37621ea596101
Akash <45712184+dev-akash-sarker@users.noreply.github.com>
2026-02-10 14:31:13 +0600

    all

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', // Your frontend URL
+    origin: 'https://possfrontend.netlify.app', // Your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Allow cookies/auth headers if needed
   });
